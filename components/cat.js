522cb6ca4a14e57904351d54b117255422a3a14a
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-12-14 11:54:22 -0500

    inner cat ears css

--- components/cat.js
+++ components/cat.js
@@ -3,7 +3,9 @@ export default function Home(props) {
     <div>
       <div style={catFaceStyle(props.catColor)}>
         <div style={catLeftEar(props.catColor)} />
+        <div style={innerLeftCatLeftEar()} />
         <div style={catRightEar(props.catColor)} />
+        <div style={innerRightCatLeftEar()} />
         <div className="cat-eyes"></div>
         <div className="cat-nose"></div>
         <div className="cat-mouth"></div>
@@ -14,8 +16,8 @@ export default function Home(props) {
 
 const catFaceStyle = (catColor) => ({
   backgroundColor: `${catColor}`,
-  width: '100px',
-  height: '100px',
+  width: '250px',
+  height: '250px',
   borderRadius: '50%',
   display: 'block',
   position: 'relative',
@@ -23,6 +25,22 @@ const catFaceStyle = (catColor) => ({
 });
 
 const catLeftEar = (catColor) => ({
+  zIndex: '-1',
+  position: 'absolute',
+  display: 'block',
+  content: '',
+  width: '0',
+  height: '0',
+  borderLeft: '75px solid transparent',
+  borderRight: '75px solid transparent',
+  borderBottom: `100px solid ${catColor}`,
+  borderRadius: '12.5px',
+  left: '-42.5px',
+  top: '-12.5px',
+  transform: 'rotate(-40deg)',
+});
+
+const innerLeftCatLeftEar = () => ({
   position: 'absolute',
   display: 'block',
   content: '',
@@ -30,11 +48,11 @@ const catLeftEar = (catColor) => ({
   height: '0',
   borderLeft: '30px solid transparent',
   borderRight: '30px solid transparent',
-  borderBottom: `40px solid ${catColor}`,
-  borderRadius: '5px',
-  left: '-15px',
-  top: '-5px',
-  transform: 'rotate(-40deg)',
+  borderBottom: `50px solid #FFC0CB`,
+  borderRadius: '12.5px',
+  left: '-5.5px',
+  top: '5px',
+  transform: 'rotate(-45deg)',
 });
 
 const catRightEar = (catColor) => ({
@@ -43,11 +61,26 @@ const catRightEar = (catColor) => ({
   content: '',
   width: '0',
   height: '0',
+  borderLeft: '75px solid transparent',
+  borderRight: '75px solid transparent',
+  borderBottom: `100px solid ${catColor}`,
+  borderRadius: '12.5px',
+  right: '-42.5px',
+  top: '-12.5px',
+  transform: 'rotate(40deg)',
+});
+
+const innerRightCatLeftEar = () => ({
+  position: 'absolute',
+  display: 'block',
+  content: '',
+  width: '0',
+  height: '0',
   borderLeft: '30px solid transparent',
   borderRight: '30px solid transparent',
-  borderBottom: `40px solid ${catColor}`,
-  borderRadius: '5px',
-  right: '-15px',
-  top: '-5px',
-  transform: 'rotate(40deg)',
+  borderBottom: `50px solid #FFC0CB`,
+  borderRadius: '12.5px',
+  right: '-5.5px',
+  top: '5px',
+  transform: 'rotate(45deg)',
 });
